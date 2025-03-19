70784ac096ca223f5a442e88eaac860f7a1848db
Christopher Braley <cbraley@protonmail.com>
2025-03-19 09:21:06 -0500

    remove old files

--- src/bronze/expectations/expectations.py
+++
@@ -1,8 +0,0 @@
-# great_expectations/expectations/bronze/basic_expectations.py
-from great_expectations.dataset import SparkDFDataset
-
-def create_bronze_expectations(df):
-    ge_df = SparkDFDataset(df)
-    ge_df.expect_column_values_to_not_be_null("id")
-    ge_df.expect_column_values_to_not_be_null("timestamp")
-    return ge_df
